033876ca879e48df1554fc05bed5b6abc7c1c9e1
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2020-08-20 23:33:31 +0300

    3. ExampleGen

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # YouTube TFX Pipelines
 
-An introductory video series to End to End Machine Learning Pipelines using Tensorflow Extended.
+An introductory video series for End to End Machine Learning Pipelines using Tensorflow Extended.
 
 ## Repository Contents
 
 The pipeline that is implemented is the: [TFX Chicago Taxi Pipeline Example](https://github.com/tensorflow/tfx/tree/master/tfx/examples/chicago_taxi_pipeline), slightly modified.
 
-| Directory | Contents                                       |
-| :-------- | :--------------------------------------------- |
-| `data`    | The CSV raw data for the taxi pipeline example |
+| Directory                 | Contents                                       |
+| :------------------------ | :--------------------------------------------- |
+| `data`                    | The CSV raw data for the taxi pipeline example |
+| `pipeline.py`             | The full, vanilla TFX pipeline                 |
+| `pipeline_example_gen.py` | Video 3: The Example Gen Component             |
---
+++ pipeline_example_gen.py
@@ -0,0 +1,91 @@
+# Copyright 2019 Google LLC. All Rights Reserved.
+# Modifications Copyright 2020 Theodoros Ntakouris
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from typing import List, Text
+
+import absl
+import tensorflow_model_analysis as tfma
+
+from tfx.components import CsvExampleGen
+from tfx.components import Evaluator
+from tfx.components import ExampleValidator
+from tfx.components import Pusher
+from tfx.components import ResolverNode
+from tfx.components import SchemaGen
+from tfx.components import StatisticsGen
+from tfx.components import Trainer
+from tfx.components import Transform
+from tfx.dsl.experimental import latest_blessed_model_resolver
+from tfx.orchestration import metadata
+from tfx.orchestration import pipeline
+from tfx.orchestration.beam.beam_dag_runner import BeamDagRunner
+from tfx.proto import pusher_pb2
+from tfx.proto import trainer_pb2
+from tfx.types import Channel
+from tfx.types.standard_artifacts import Model
+from tfx.types.standard_artifacts import ModelBlessing
+from tfx.utils.dsl_utils import external_input
+
+_pipeline_name = 'chicago_taxi_beam'
+
+_data_root = os.path.join(os.path.dirname(__file__), 'data', 'simple')
+
+_tfx_root = os.path.join(os.path.dirname(__file__))
+_pipeline_root = os.path.join(os.path.dirname(__file__), 'pipeline_out', _pipeline_name)
+
+_metadata_path = os.path.join(_tfx_root, 'metadata.db')
+
+_beam_pipeline_args = [
+    '--direct_running_mode=multi_processing',
+    # 0 means auto-detect based on on the number of CPUs available
+    # during execution time.
+    '--direct_num_workers=0',
+]
+
+
+def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
+                     metadata_path: Text,
+                     beam_pipeline_args: List[Text]) -> pipeline.Pipeline:
+  """Implements the chicago taxi pipeline with TFX."""
+  examples = external_input(data_root)
+
+  # Brings data into the pipeline or otherwise joins/converts training data.
+  example_gen = CsvExampleGen(input=examples)
+
+  return pipeline.Pipeline(
+      pipeline_name=pipeline_name,
+      pipeline_root=pipeline_root,
+      components=[
+          example_gen
+      ],
+      enable_cache=True,
+      metadata_connection_config=metadata.sqlite_metadata_connection_config(
+          metadata_path),
+      beam_pipeline_args=beam_pipeline_args)
+
+
+# To run this pipeline from the python CLI:
+#   $python taxi_pipeline_beam.py
+if __name__ == '__main__':
+  absl.logging.set_verbosity(absl.logging.INFO)
+
+  BeamDagRunner().run(
+      _create_pipeline(
+          pipeline_name=_pipeline_name,
+          pipeline_root=_pipeline_root,
+          data_root=_data_root,
+          metadata_path=_metadata_path,
+          beam_pipeline_args=_beam_pipeline_args))
